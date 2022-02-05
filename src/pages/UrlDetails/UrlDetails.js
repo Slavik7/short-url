f536a65c4fbb46112876b050b6eb806ee07e6515
Slavik7 <slavikloz91@gmail.com>
2022-02-05 19:47:51 +0200

    update

--- src/pages/UrlDetails/UrlDetails.js
+++ src/pages/UrlDetails/UrlDetails.js
@@ -6,22 +6,29 @@ import * as S from "./style";
 const UrlDetails = () => {
   const { id } = useParams();
   const [urlInfo, setUrlInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [ready, setReady] = useState(false);
   useEffect(() => {
     const getData = async () => {
       const data = await getDataById(id);
       if (data) {
         setUrlInfo({ ...data });
       }
-      setLoading(false);
+      setReady(true);
     };
     getData();
   }, []);
   return (
     <S.DetailsPage>
       <h2>Short URL Information</h2>
-      {!loading &&
-        (urlInfo ? <Details urlInfo={urlInfo} /> : <h3>URL not found</h3>)}
+      {ready ? (
+        urlInfo ? (
+          <Details urlInfo={urlInfo} />
+        ) : (
+          <h3>URL not found</h3>
+        )
+      ) : (
+        <h3>Loading...</h3>
+      )}
     </S.DetailsPage>
   );
 };
