b42885f6a06c419658b370cd08d39895e75d1224
Slavik7 <slavikloz91@gmail.com>
2022-02-05 14:20:36 +0200

    first commit

--- src/util/data.js
+++
@@ -1,9 +0,0 @@
-export const incEntries = ({ id }) => {
-  const data = JSON.parse(localStorage.getItem("data"));
-  if (data) {
-    const newData = data.map((url) => {
-      if (url.id === id) url.entries++;
-      return url;
-    });
-  }
-};
